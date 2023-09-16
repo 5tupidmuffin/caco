6e325c8d9d509645bac13ffb4fd0a7767b499462
5tupidmuffin <rameshwark9500@gmail.com>
2023-09-16 20:20:21 +0530

    more pwa changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 	if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
 		navigator.serviceWorker.register('service-worker.js', {
-			scope: '/caco',
+			scope: '/caco/',
 			type: dev ? 'module' : 'classic'
 		});
 	}
